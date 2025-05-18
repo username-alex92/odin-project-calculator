f9308c68d325b284b2b130664b23715ab8b2fee4
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-19 01:07:28 +0300

    Add event listenersfor the calculator to work

--- script.js
+++ script.js
@@ -15,13 +15,41 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(firstNumber, operate, secondNumber) {
-    if (operate === "+") return add(firstNumber, secondNumber);
-    if (operate === "-") return subtract(firstNumber, secondNumber);
-    if (operate === "*") return multiply(firstNumber, secondNumber);
-    if (operate === "/") return divide(firstNumber, secondNumber);
+function operate(firstNumber, operator, secondNumber) {
+    if (operator === "+") return add(firstNumber, secondNumber);
+    if (operator === "-") return subtract(firstNumber, secondNumber);
+    if (operator === "*") return multiply(firstNumber, secondNumber);
+    if (operator === "/") return divide(firstNumber, secondNumber);
 }
+
+
+let input = document.querySelector("input");
+let digits = document.querySelector(".digits");
+let operators = document.querySelector(".operators");
+let equals = document.querySelector("#equals");
 
 let firstNumber;
 let secondNumber;
 let operator;
+let result;
+
+
+digits.addEventListener("click", (event) => {
+    if (result !== undefined) {
+        input.value = "";
+        result = undefined;
+    }
+    input.value += event.target.textContent;
+});
+
+operators.addEventListener("click", (event) => {
+    firstNumber = input.value;
+    input.value = "";
+    operator = event.target.textContent;
+});
+
+equals.addEventListener("click", () => {
+    secondNumber = input.value;
+    result = operate(parseInt(firstNumber), operator, parseInt(secondNumber));
+    input.value = result;
+});
